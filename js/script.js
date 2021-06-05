9134cd1018e4869ed4ec5514e9acbfef9135c3d2
ronnielopez <ronnielopez503@gmail.com>
2021-06-05 15:55:05 -0600

    letras animacion

--- js/script.js
+++ js/script.js
@@ -247,3 +247,19 @@ function wheel(e) {
 }
 window.addEventListener('mousewheel', wheel, false);
 window.addEventListener('wheel', wheel, false);
+
+
+setInterval(function () {
+  const show = document.querySelector('span[data-show]')
+  const next = show.nextElementSibling || document.querySelector('span:first-child')
+  const up = document.querySelector('span[data-up]')
+  
+  if (up) {
+    up.removeAttribute('data-up')
+  }
+  
+  show.removeAttribute('data-show')
+  show.setAttribute('data-up', '')
+  
+  next.setAttribute('data-show', '')
+}, 2000)
